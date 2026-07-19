8fe80e80c04429fcc8b4f37d03bf9dc22a2f8566
adityagourshete6c-crypto <adityagourshete6c@gmail.com>
2026-07-19 16:51:28 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
-  return {
+  return {base:'/Inventory-Managememt/'
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
